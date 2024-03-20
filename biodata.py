f543333d2aaf5712149c570f9e9ff5531eb26fd0
muhagunghanapi <161696731+muhagunghanapi@users.noreply.github.com>
2024-03-20 23:55:51 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,5 @@
+nama = input("masukkan nama : ")
+tgl = input("masukkan tanggal lahir : ")
+hobi = input("masukkan hobi : ")
+pemisah = tgl.split("")
+print("tanggal {pemisah[0]}, bulan {pemisah[1]}, tahun {pemisah[2]}")
